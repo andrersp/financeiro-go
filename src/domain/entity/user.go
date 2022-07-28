fd18e8d1d94b8695ec25956744b6acd65cba507d
Andre Luis <andreluis@MacBook-Air-de-Andre.local>
2022-07-28 11:03:34 -0300

    TEST: start TDD

--- src/domain/entity/user.go
+++ src/domain/entity/user.go
@@ -12,12 +12,12 @@ import (
 )
 
 type User struct {
-	ID        uint64    `json:"id" `
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        uint64    `gorm:"primary_key;auto_increment" json:"id" `
+	FirstName string    `gorm:"size:100;not null;" json:"first_name"`
+	LastName  string    `gorm:"size:100;not null;" json:"last_name"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 }
 
 type PublicUser struct {
